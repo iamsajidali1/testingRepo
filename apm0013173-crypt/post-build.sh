346bef8a1ccf18803d3f9964d70db5200d6ecf54
Sajid <iamsajidali543@gmail.com>
2026-03-04 15:13:06 +0530

    deleted crypt

--- apm0013173-crypt/post-build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-set -ex
-image=$(docker images|grep $1|awk '{print $1 ":" $2}'|head -n 1)
-docker run -i --network none --env DJANGO_SECRET=test ${image} /app/test.sh
--- apm0013173-crypt/run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-set -xe
-
-python3 manage.py collectstatic
-nginx -c $APP_DIR/nginx.conf
-uwsgi --ini $APP_DIR/uwsgi.ini > /dev/stdout
